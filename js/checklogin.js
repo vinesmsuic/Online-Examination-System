1bff191fda494b70e7d8a511e48efa8317722d4c
Vines <34955859+vinesmsuic@users.noreply.github.com>
2020-12-07 00:01:13 +0800

    up

--- js/checklogin.js
+++ js/checklogin.js
@@ -1,16 +1,16 @@
-function updateList() {
-    var Request = new XMLHttpRequest();
-    Request.open("GET", "http://localhost/web/Online-Examination-System/main.html", true);
-    Request.send();
-    Request.onload = function() {
-        var respond = Request.responseText;
-        if (respond == "fail to find account") {
-
-        } else if (respond == "password not matching") {
-
-        } else if (respond == "success") {
-
+function login() {
+    if ((password != "") && (enterID != "")) {
+        var Request = new XMLHttpRequest();
+        var info = "?enterID=" + document.getElementById("enterID").value + "&password=" + document.getElementById("password").value;
+        Request.open("GET", "http://localhost/web/Online-Examination-System/php/login.php" + info, true);
+        Request.send();
+        Request.onload = function() {
+            var respond = Request.responseText;
+            if ((respond == "fail to find account") || (respond == "password not matching")) {
+                document.getElementById("errorMessage").innerText = respond;
+            } else if (respond == "success") {
+                window.location.href = "http://www.w3schools.com";
+            }
         }
-        document.getElementById("eIDList").innerHTML = optionList;
     }
 }
--- php/login.php
+++ php/login.php
@@ -4,9 +4,9 @@
 		if(!$connect){
 			die("Connection failed: ".mysqli_connect_error());
         }
-        $ID = $_POST['enterID'];
-        $PW = $_POST['password'];
-        $Query = "SELECT pw, userType FROM users WHERE ID = '$ID'";
+        $ID = $_GET['enterID'];
+        $PW = $_GET['password'];
+        $Query = "SELECT PW, userType FROM users WHERE ID = '$ID'";
 	    $Result = mysqli_query($connect, $Query);
 	    if (!$Result){
 	    	die("Could not successfully run query.". mysqli_error($connect));
@@ -15,9 +15,9 @@
 	    	echo "fail to find account";//display message of no such student/teacher/admin
 	    } else {
             $row = mysqli_fetch_assoc($Result);
-            if ($PW == $row['pw']) {
+            if ($PW == $row['PW']) {
                 $type = $row['userType'];
-                setcookie($ID, $type, time() + (60*60*24));//save data, record cookie for 24hours
+                setcookie($ID, $type, time() + 1);//save data, record cookie for 24hours
                 echo "success";//login success
             } else {
                 echo "password not matching";//display message of password error
