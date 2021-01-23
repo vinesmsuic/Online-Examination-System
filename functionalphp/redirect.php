9958f6259f8d4d07b3b2742c721218ed388c3a91
Michelle5757 <77832753+Michelle5757@users.noreply.github.com>
2021-01-23 10:20:39 +0200

    Update redirect.php
    CHanged to use SESSION and not cookies

--- functionalphp/redirect.php
+++ functionalphp/redirect.php
@@ -1,9 +1,9 @@
 <?php
-    if (isset($_COOKIE["type"])){
-        setcookie("type", $_COOKIE["type"], time() + 21600, "/");
-        setcookie("userID", $_COOKIE["userID"], time() + 21600, "/");
-        setcookie("nickName", $_COOKIE["nickName"], time() + 21600, "/");
-        echo $_COOKIE["type"];
+    if (isset($_SESSION["type"])){
+//       setcookie("type", $_COOKIE["type"], time() + 21600, "/");
+//       setcookie("userID", $_COOKIE["userID"], time() + 21600, "/");
+//       setcookie("nickName", $_COOKIE["nickName"], time() + 21600, "/");
+        echo $_SESSION["type"];
     } else {
         echo "not logged.";
     }
